b3267b0361e54f06ac9a4256e874e72be02c1ebf
Jenya Kovalev <woodmoure@gmail.com>
2025-06-18 14:54:18 +0300

    added Reset Button

--- src/components/CounterBox.tsx
+++ src/components/CounterBox.tsx
@@ -10,12 +10,16 @@ export default function CounterBox() {
   function handleMinusCounter() {
     setCounter(() => counter - 1);
   }
+  function handleReset() {
+    setCounter(() => 0);
+  }
 
   return (
     <>
       <h1>{counter}</h1>
       <Button onHandleFunc={handlePlusCounter}>Plus</Button>
       <Button onHandleFunc={handleMinusCounter}>Minus</Button>
+      <Button onHandleFunc={handleReset}>Reset</Button>
     </>
   );
 }
